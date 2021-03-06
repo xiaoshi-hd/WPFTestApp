1e385e4f7089abe4ab236beab1dc59240834a97a
小师 <1209001368@qq.com>
2021-03-06 16:14:55 +0800

    添加项目文件。

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Student student;
+
+        public MainWindow()
+        {
+            InitializeComponent();  // 初始化UI元素
+        }
+
+        private void buttonClick(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
---
+++ WpfApp1/Student.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+
+namespace WpfApp1
+{
+    internal class Student : INotifyPropertyChanged  // 继承接口
+    {
+        private string name;
+        //private string Name { get; set; } // 自动属性，相当于name字段加了get、set方法
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // 设置get、set方法，使用时Student.Name = XXX即调用set；XXX = Student.Name调用get
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                // 激发事件
+                if (this.PropertyChanged != null)
+                {
+                    this.PropertyChanged.Invoke(this, new PropertyChangedEventArgs("Name"));
+                }
+            }
+        }
+    }
+}
